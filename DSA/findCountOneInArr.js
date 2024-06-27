2065cec4aa76b0aa39d0f778d2eaf6f9f25384ed
shubham <shubham@blokument.com>
2024-06-28 01:56:04 +0530

    find element that appears only in once

---
+++ DSA/findCountOneInArr.js
@@ -0,0 +1,22 @@
+//Find the element that appears once in an array
+//where every other element appears twice
+
+//Input:  arr[] = {2, 3, 5, 4, 5, 3, 4}
+// Output: 2 
+
+const findSingleCount = (arr) => {
+    let count = arr?.reduce((acc, curr) => {
+        acc[curr] = (acc[curr] || 0) + 1
+        return acc
+    }, {})
+    console.log("Count", count)
+
+    let formattedArr = Object.keys(count)?.map((k) => {
+        return {
+            key: k,
+            repeat: count[k]
+        }
+    })
+
+    return formattedArr?.filter((f) => f?.repeat === 1)
+}
