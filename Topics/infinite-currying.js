49dc460c8852404a28022b13ac90130832faaf19
shubham <shubham@blokument.com>
2024-06-29 02:47:44 +0530

    Theory Topics added

---
+++ Topics/infinite-currying.js
@@ -0,0 +1,20 @@
+function add1(a){
+	return function(b){
+		return a+b;
+	}
+}
+
+console.log(add1(2)(3));
+
+
+
+function add(a){
+	return function(b){
+  	if(b !== undefined){
+    	return add(a+b)
+    }
+  	return a;
+  }
+}
+
+console.log(add(2)(3)(6)()); 11
