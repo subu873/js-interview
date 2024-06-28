68522f349b4920787625788ee76c89e25ecfe2ac
shubham <shubham@blokument.com>
2024-06-29 00:30:19 +0530

    array method practise set questions

---
+++ DSA/question.js
@@ -0,0 +1,19 @@
+/*
+Practise these questions
+1.Write a function that takes a string as input and returns the reverse of that string using JavaScript.
+2.Implement a function to remove all duplicates from an array using JavaScript.
+3.Write a JavaScript function to find the maximum and minimum elements in an array.
+4.Implement a function that checks if a given string is a palindrome using JavaScript.
+5.Write a function to find the factorial of a given number using JavaScript.
+6.Implement a function to sort an array of numbers in ascending order using JavaScript.
+7.Write a JavaScript function to check if a given string is a valid email address.
+8.Implement a function to check if a given number is prime or not using JavaScript.
+9.Write a JavaScript function to find the sum of all the elements in an array.
+10.Implement a function to calculate the area of a triangle given the base and height using JavaScript.
+11.Write a function that takes two arrays as input and returns their intersection using JavaScript.
+12.Implement a function that checks if a given number is even or odd using JavaScript.
+13.Write a JavaScript function to find the nth Fibonacci number.
+14.Implement a function to find the largest sum of any two elements in an array using JavaScript.
+15.Write a function to convert a given string to title case using JavaScript.
+
+*/
