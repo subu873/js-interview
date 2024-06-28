06bdce9df9fd32c932ff3a3d9b90589572ee6cc9
shubham <shubham@blokument.com>
2024-06-29 02:28:38 +0530

    currying question sum added

---
+++ DSA/currying.js
@@ -0,0 +1,24 @@
+//sum(1)(2)(3) == 6
+// sum(1)(2)(3)() === 6
+
+const sum = a => b => c => a + b + c
+
+function sum(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c
+        }
+    }
+}
+
+// sum(1)(2)(3)() === 6
+
+function sumCall(a) {
+    return function (b) {
+        if (b) {
+            return sumCall(a + b)
+        } else {
+            return a
+        }
+    }
+}
